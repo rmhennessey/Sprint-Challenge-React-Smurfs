17490467e266fddc68a219c6cf0aafb3f73792fa
RMH <rory.hennessey@gmail.com>
2019-03-01 11:50:51 -0500

    mvp - can post new smurf, routes are implemented, navlinks are implemented. nothing is styled

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Link, NavLink } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -22,14 +23,44 @@ class App extends Component {
       })
       .catch(error => console.log(error));
   }
+
+  postSmurf = (smurf) => {
+    console.log(smurf)
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(response => {
+        this.setState({ smurfs: response.data})
+      })
+      .catch(error => console.log(error));
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav>
+          <NavLink to='/'>Smurfs</NavLink>
+          <NavLink to='/smurf-form'>Add a Smurf</NavLink>
+
+        </nav>
+
+        <Route 
+          path='/smurf-form'
+          exact
+          render={ (props) => (
+          <SmurfForm 
+          {...props}
+          postSmurf={this.postSmurf} /> 
+        )}/>
+        <Route
+          path='/'
+          exact
+          render= { (props) => (
+          <Smurfs 
+          {...props}
+          smurfs={this.state.smurfs} />
+          )}/>
       </div>
     );
   }
